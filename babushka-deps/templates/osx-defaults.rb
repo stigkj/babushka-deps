e9c428d89b421ce4387429db37a0531203d24cb2
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2014-09-01 16:14:30 +0200

    Add template for the OS X defaults system

---
+++ babushka-deps/templates/osx-defaults.rb
@@ -0,0 +1,33 @@
+meta :osx_defaults do
+  accepts_value_for :setting
+  accepts_value_for :type
+  accepts_value_for :value
+
+  template do
+    met? {
+      status = `defaults read #{setting}`.strip
+      status == value
+    }
+    
+    meet {
+      shell "defaults write #{setting} -#{type} #{value}"
+    }
+  end
+end
+
+dep "dashboard.osx_defaults" do
+  setting "com.apple.dashboard mcx-disabled"
+  type "bool"
+  value "1"
+
+  after {
+    shell "killall Dock"
+  }
+end
+
+dep "current dir finder scope.osx_defaults" do
+  setting "com.apple.finder FXDefaultSearchScope"
+  type "string"
+  value "SCcf"
+end
+
