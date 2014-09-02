9c87657de9e409552d1052a02d26a62404661968
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2014-09-02 13:49:11 +0200

    Fix spelling
    Add missing 't'.

--- babushka-deps/deps/development/chruby.rb
+++ babushka-deps/deps/development/chruby.rb
@@ -1,5 +1,5 @@
 dep "chruby.managed" do
-  # This is because Babushka doesn't understand chruby because it's an expored
+  # This is because Babushka doesn't understand chruby because it's an exported
   # bash function.
   met? {
     "~/.rubies".p.exists?
