95105e0f09edaaeca1f93d88ecf9764265297024
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2014-09-04 12:29:48 +0200

    Fix spelling (or --> for)

--- deps/hacks/enable_access_for_assistive_devices.rb
+++ deps/hacks/enable_access_for_assistive_devices.rb
@@ -1,4 +1,4 @@
-dep 'Enable access or assistive devices', :for => :osx do
+dep 'Enable access for assistive devices', :for => :osx do
   met? { File.exists? '/private/var/db/.AccessibilityAPIEnabled' }
   meet { shell('touch /private/var/db/.AccessibilityAPIEnabled', :sudo => true) }
 end
