1a861991b98a863d6a07d491de2e68865b061b80
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2014-09-04 12:31:26 +0200

    Should use latest unreleased version of mackup

--- deps/utilities/mackup.rb
+++ deps/utilities/mackup.rb
@@ -1,10 +1,13 @@
 dep 'mackup' do
   requires 'Dropbox',
-    'mackup.managed',
+    'mackup HEAD',
     'Make sure mackup backup or restore is run before continuing'
 end
 
-dep 'mackup.managed'
+dep 'mackup HEAD' do
+  met? { Babushka::BrewHelper.has? 'mackup' }
+  meet { log_shell 'Installing mackup', 'brew install mackup --HEAD' }
+end
 
 dep 'Make sure mackup backup or restore is run before continuing' do
   meet { Babushka::Prompt.confirm('Run mackup backup or restore now in another shell before pressing enter', :default => 'y') }
