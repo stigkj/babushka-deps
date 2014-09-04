4d042bdd3a1792b695f0cec4933f378dad26508c
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2014-09-04 23:35:06 +0200

    Only restore should be run, not backup
    When mackup is installed, almost nothing else has been installed
    so there is no need for doing a mackup backup.

--- deps/utilities/mackup.rb
+++ deps/utilities/mackup.rb
@@ -1,7 +1,7 @@
 dep 'mackup' do
   requires 'Dropbox',
     'mackup HEAD',
-    'Make sure mackup backup or restore is run before continuing'
+    'Make sure mackup restore is run before continuing'
 end
 
 dep 'mackup HEAD' do
@@ -9,6 +9,6 @@ dep 'mackup HEAD' do
   meet { log_shell 'Installing mackup', 'brew install mackup --HEAD' }
 end
 
-dep 'Make sure mackup backup or restore is run before continuing' do
-  meet { Babushka::Prompt.confirm('Run mackup backup or restore now in another shell before pressing enter', :default => 'y') }
+dep 'Make sure mackup restore is run before continuing' do
+  meet { Babushka::Prompt.confirm('Run mackup restore now in another shell before pressing enter', :default => 'y') }
 end
