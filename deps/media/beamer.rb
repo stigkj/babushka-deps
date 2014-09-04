2b8bc1e13d6cd84a40c7af2d248f8f8c66aace86
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2014-09-04 09:54:28 +0200

    Add Beamer Airplay streamer app

--- deps/media/beamer.rb
+++ deps/media/beamer.rb
@@ -1,6 +1,1 @@
-dep 'beamer.bin' do
-  installs {
-    # Extend cask template to take installs and via like bin does
-    via :mountain_lion, 'beamer.cask'
-  }
-end
+dep 'beamer.cask'
--- roles/media/osx.rb
+++ roles/media/osx.rb
@@ -1,2 +1,3 @@
 dep 'OS X media', :for => :osx do
+  requires 'beamer.cask'
 end
