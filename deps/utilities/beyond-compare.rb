724dfb7ca9ae6d058309d5cfec42e99241efc6d3
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2014-09-08 23:55:04 +0200

    Link in Beyond Compare utils into path

--- deps/utilities/beyond-compare.rb
+++ deps/utilities/beyond-compare.rb
@@ -4,6 +4,35 @@ dep 'Beyond Compare' do
     # TODO setup for Linux too
     otherwise { unsupported_platform! }
   }
+  requires 'put BCompare and bcomp in path'
 end
 
 dep 'beyond-compare.cask', :for => :osx
+
+dep 'put BCompare and bcomp in path' do
+  met? { binaries.all? { |binary| "#{bin_path}/#{binary}".p.exists? } }
+  meet {
+    log_block "Linking Beyond Compare command line utilities #{binaries} into #{bin_path}" do
+      binaries.each { |binary|
+        shell %Q{ln -sf "#{path_to_binaries}/#{binary}" "#{bin_path}/#{binary}" }
+      }
+    end
+  }
+end
+
+def binaries
+  %w[BCompare bcomp]
+end
+
+# We assume babushka's method for finding a directory that is in path is ok
+def bin_path
+  "/usr/local/bin".p
+end
+
+def path_to_binaries
+  "#{caskroom}/beyond-compare/4.0.0.18847/Beyond Compare.app/Contents/MacOS/".p
+end
+
+def caskroom
+  "/opt/homebrew-cask/Caskroom".p
+end
