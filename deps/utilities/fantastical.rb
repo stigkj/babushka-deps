616d1db43d10ca1c09c7a1fd77993fefcbf92c13
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2014-09-16 07:08:16 +0200

    Add Fantastical application

---
+++ deps/utilities/fantastical.rb
@@ -0,0 +1,1 @@
+dep 'fantastical.cask', :for => :osx
--- roles/utilities/osx_utilities.rb
+++ roles/utilities/osx_utilities.rb
@@ -1,6 +1,7 @@
 dep 'OS X utilities', :for => :osx do
   requires 'onepassword.cask',
            'alfred.cask',
+           'fantastical.cask',
            'stay.cask',
            'ipartition.cask',
            'choosy.cask'
