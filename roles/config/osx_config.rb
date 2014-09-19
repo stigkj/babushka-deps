46d69e0616db9f54e29f19b80f28d0d47ce9878c
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2014-09-19 14:56:50 +0200

    Add TODO for setting Chrome as default browser

--- roles/config/osx_config.rb
+++ roles/config/osx_config.rb
@@ -25,6 +25,7 @@ dep 'OS X config', :for => :osx do
            'Enable right click with two finger single tap',
            'Enable password-protected screensaver'
   #TODO change keyboard shortcuts for Alfred and Spotlight
+  #TODO set Chrome as default browser by using https://github.com/kerma/defaultbrowser/blob/master/build/defaultbrowser
 
   met? { marker_file.exists? }
   meet {
