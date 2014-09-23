c8fdaf56fe1997f82ba97e302c3e8b8b72f51e24
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2014-09-23 10:27:25 +0200

    Add Groovy

---
+++ deps/development/groovy.rb
@@ -0,0 +1,1 @@
+dep 'groovy.managed'
--- roles/development/common_development.rb
+++ roles/development/common_development.rb
@@ -2,6 +2,7 @@ dep 'common development' do
   # TODO install into Development dir
   requires 'Java 8',
            'gradle.managed',
+           'groovy.managed',
            'Tomcat 7',
            'IntelliJ IDEA 14 EAP',
            'ruby'
