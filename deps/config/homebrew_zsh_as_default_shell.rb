bc6896b742c69a2ca4a93ad1b10a3662393cc0ab
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2014-11-11 11:22:09 +0100

    Improve name of dependency
    Also use the sudo version of running a shell script in addition to
    improve formatting.

--- deps/config/homebrew_zsh_as_default_shell.rb
+++ deps/config/homebrew_zsh_as_default_shell.rb
@@ -1,9 +1,6 @@
-dep 'Setup shell' do
-  requires 'Set Homebrew zsh as default shell'
-end
-
-dep 'Set Homebrew zsh as default shell' do
-  requires 'Add Homebrew zsh to legal shells'
+dep 'Homebrew zsh as default shell' do
+  requires 'zsh.managed',
+           'Add Homebrew zsh to legal shells'
 
   met? { shell('dscl . -read /Users/stiklepp UserShell').include? homebrew_zsh_location }
   meet { log_shell 'Setting Homebrew zsh as default shell', "chsh -s #{homebrew_zsh_location}" }
@@ -11,7 +8,11 @@ end
 
 dep 'Add Homebrew zsh to legal shells' do
   met? { '/etc/shells'.p.read.include? homebrew_zsh_location }
-  meet { log_shell 'Adding Homebrew zsh to legal shells', "echo #{homebrew_zsh_location} | sudo tee -a /etc/shells" }
+  meet {
+    log_shell 'Adding Homebrew zsh to legal shells',
+              "echo #{homebrew_zsh_location} >> /etc/shells",
+              :sudo => true
+  }
 end
 
 def homebrew_zsh_location
