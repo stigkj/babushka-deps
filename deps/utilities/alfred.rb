26d9315929987aa46afe79d2e466660db1161e7c
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2014-11-11 22:13:23 +0100

    Use accessibility api meta dep on Alfred and Stay

--- deps/utilities/alfred.rb
+++ deps/utilities/alfred.rb
@@ -1,1 +1,7 @@
-dep 'alfred.cask', :for => :osx
+dep 'Alfred', :for => :osx do
+  requires 'alfred.cask',
+           'com.runningwithcrayons.Alfred-2.accessibility_api'
+end
+
+dep 'alfred.cask'
+dep 'com.runningwithcrayons.Alfred-2.accessibility_api'
