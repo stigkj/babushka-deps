5d661120a1321a278421a64ad9fc9643b555c7e5
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2014-11-12 14:54:13 +0100

    Fix name of IntelliJ IDEA Ultimate EAP dependency
    intellij-idea-ultimate-eap.cask --> intellij-idea-eap.cask

--- deps/development/intellij14_eap.rb
+++ deps/development/intellij14_eap.rb
@@ -1,10 +1,10 @@
 dep 'IntelliJ IDEA 14 EAP' do
   requires 'caskroom versions'
   requires {
-    on :osx, 'intellij-idea-ultimate-eap.cask'
+    on :osx, 'intellij-idea-eap.cask'
     otherwise { unsupported_platform! }
   }
 end
 
 # TODO change memory configuration in plist
-dep 'intellij-idea-ultimate-eap.cask', :for => :osx
+dep 'intellij-idea-eap.cask', :for => :osx
