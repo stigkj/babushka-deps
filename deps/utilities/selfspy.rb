e4bdd9111b60ec290e6c375ec0e969d44b2b9867
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2014-11-12 15:30:29 +0100

    Enable accessibility api for selfspy

--- deps/utilities/selfspy.rb
+++ deps/utilities/selfspy.rb
@@ -1,3 +1,10 @@
 # selfspy stores all keypresses and mouse activity and makes it searchable
 
+dep 'selfspy' do
+  requires 'selfspy.pip',
+           'com.github.gurgeh.selfspy.accessibility_api'
+end
+
 dep 'selfspy.pip'
+
+dep 'com.github.gurgeh.selfspy.accessibility_api'
