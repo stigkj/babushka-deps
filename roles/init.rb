56ea3306387d51159b9d91a232a9efb5d18a452a
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2014-11-16 10:04:52 +0100

    Fix partitioning id (disk0s2 -> disk1)
    Yosemite has a different partitioning scheme, so the disk where
    everything is located has now the identifier disk1 instead of
    disk0s2.

--- roles/init.rb
+++ roles/init.rb
@@ -8,6 +8,6 @@ end
 dep 'Convert HD to case-sensitive format', :for => :osx do
   requires_when_unmet 'iPartition.app'
 
-  met? { shell('diskutil info disk0s2').include? 'Case-sensitive' }
+  met? { shell('diskutil info disk1').include? 'Case-sensitive' }
   meet { Babushka::Prompt.confirm('Start iPartition and convert partition to case-sensitive format before going any further', :default => 'y') }
 end
