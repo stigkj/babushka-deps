01aaf54ba52492437e57c666f5514ed97577e11d
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2014-11-17 20:42:31 +0100

    No need to use sudo for Gatekeeper status

--- deps/config/turn_off_gatekeeper_for_everything.rb
+++ deps/config/turn_off_gatekeeper_for_everything.rb
@@ -1,4 +1,4 @@
 dep 'Turn off Gatekeeper for all applications' do
-  met? { !shell?('spctl --status', :sudo => true) }
+  met? { !shell? 'spctl --status' }
   meet { log_shell 'Turning off Gatekeeper for all applications', "spctl --master-disable", :sudo => true }
 end
