248a3f8d4cdf12615c8401d1e130d6855cd8073d
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2014-11-18 16:01:22 +0100

    Set host name to user name without finn.no
    Also add logging and extract user name as it is used several times.

--- deps/config/fix_hostname_bug.rb
+++ deps/config/fix_hostname_bug.rb
@@ -1,12 +1,15 @@
 dep 'Fix hostname-through-DHCP bug', :for => :osx do
-  hostname = "#{ENV['USER']}.finn.no"
+  username = ENV['USER']
+  hostname = "#{username}.finn.no"
 
   met? {
     File.readlines("/etc/hosts").grep(/#{hostname}/).any?
   }
 
   meet {
-    shell("echo '127.0.0.1 #{hostname}' >> /etc/hosts", :sudo => true)
-    shell("scutil --set HostName #{hostname}", :sudo => true)
+    log_block 'Fixing hostname-through-DHCP bug' do
+      shell "echo '127.0.0.1 #{hostname}' >> /etc/hosts", :sudo => true
+      shell "scutil --set HostName #{username}", :sudo => true
+    end
   }
 end
