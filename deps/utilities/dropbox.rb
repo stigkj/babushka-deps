d384cf7cb906fdb1e4b64c404185118dab3eba8e
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2014-11-18 11:55:50 +0100

    Improve name of dependency

--- deps/utilities/dropbox.rb
+++ deps/utilities/dropbox.rb
@@ -4,14 +4,14 @@ dep 'Dropbox' do
     # TODO setup for Linux too
     otherwise { unsupported_platform! }
   }
-  requires 'Make sure Dropbox is initialized before continuing'
+  requires 'Dropbox must be initialized before continuing'
 end
 
 dep 'Dropbox.app', :for => :osx do
   source 'https://d1ilhw0800yew8.cloudfront.net/client/Dropbox%202.10.51.dmg'
 end
 
-dep 'Make sure Dropbox is initialized before continuing' do
+dep 'Dropbox must be initialized before continuing' do
   met? { '~/Dropbox'.p.exists? }
   meet {
     Process.spawn('open /Applications/Dropbox.app')
