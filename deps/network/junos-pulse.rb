6dbc22587c55802a445ed6082ac4e9df62070307
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2015-02-21 02:47:13 +0100

    Add file for VPN to work

--- deps/network/junos-pulse.rb
+++ deps/network/junos-pulse.rb
@@ -1,3 +1,23 @@
+dep 'Junos Pulse' do
+  requires 'JunosPulse.installer',
+           'Create VPN check file'
+end
+
 dep 'JunosPulse.installer', :for => :osx do
   source "file://#{ENV['HOME']}/Dropbox/JunosPulse313.dmg.zip"
+end
+
+dep 'Create VPN check file' do
+  met? { File.exists? vpn_check_file }
+  meet {
+    log_block 'Creating VPN check file' do
+      shell "mkdir -p #{vpn_check_file.parent}", :sudo => true
+      shell "cp #{ENV['HOME']}/Dropbox/mac #{vpn_check_file}", :sudo => true
+      shell "chown #{ENV['USER']} #{vpn_check_file}", :sudo => true
+    end
+  }
+
+  def vpn_check_file
+    '/private/etc/aft/mac.check'.p
+  end
 end
