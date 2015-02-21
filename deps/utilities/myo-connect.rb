4d5a4dbc7f912a194835063245e76efe9ddff84a
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2015-02-22 00:16:41 +0100

    Add Myo Connect

---
+++ deps/utilities/myo-connect.rb
@@ -0,0 +1,7 @@
+dep 'Myo Connect', :for => :osx do
+  requires 'myo-connect.cask',
+           'com.thalmic.myoconnect.accessibility_api'
+end
+
+dep 'myo-connect.cask', :for => :osx
+dep 'com.thalmic.myoconnect.accessibility_api'
