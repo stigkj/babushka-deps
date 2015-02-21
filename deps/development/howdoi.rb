4c997b18c7ba46ce62f3ff650318188435bde9a1
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2015-02-22 00:09:09 +0100

    Add howdoi; search SO from the command line

---
+++ deps/development/howdoi.rb
@@ -0,0 +1,19 @@
+# Intelligent search of StackOverflow from the command line
+dep 'howdoi' do
+  requires 'howdoi.pip',
+           'howdoi.shell_config'
+end
+
+dep 'howdoi.pip'
+
+dep 'howdoi.shell_config' do
+  must_include 'HOWDOI_COLORIZE'
+  content_to_append shell_config
+end
+
+def shell_config
+  <<-EOF.unindent
+    # Inserted by howdoi dependency in Babushka
+    export HOWDOI_COLORIZE=true
+  EOF
+end
