75c83c6dda0d7b6d1e7d655e4eca8bdb0b9855b3
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2015-03-27 22:39:44 +0100

    Must setup ssh config before cloning from github

--- deps/utilities/mackup.rb
+++ deps/utilities/mackup.rb
@@ -5,10 +5,20 @@ dep 'mackup' do
 end
 
 dep 'mackup HEAD' do
+  requires 'Setup ssh config from Dropbox-Mackup'
   requires 'binary.my_forked_homebrew'
 
   met? { Babushka::BrewHelper.has? 'mackup' }
   meet { log_shell 'Installing mackup', 'brew install mackup --HEAD' }
+end
+
+dep 'Setup ssh config from Dropbox-Mackup' do
+  met? { '~/.ssh'.p.symlink? }
+  meet { 
+    log_block 'Setting up ssh config' do
+      shell 'ln -nsf ~/Dropbox/Mackup/.ssh ~/.ssh && chmod 700 ~/.ssh && chmod 600 ~/.ssh/*'
+    end
+  }
 end
 
 dep 'Mackup restore must run before continuing' do
