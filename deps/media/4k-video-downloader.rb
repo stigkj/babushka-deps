6ffa19544858e24a17f6b6a30ce4ab4731a0ef40
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2015-03-27 23:20:00 +0100

    Fix name of 4k video downloader

--- deps/media/4k-video-downloader.rb
+++ deps/media/4k-video-downloader.rb
@@ -1,10 +1,10 @@
 # Media downloader for sites like Youtube, Vimeo, etc
 dep '4K Video Downloader' do
   requires {
-    on :osx, 'fourk-video-downloader.cask'
+    on :osx, '4k-video-downloader.cask'
     # TODO setup for Linux too
     otherwise { unsupported_platform! }
   }
 end
 
-dep 'fourk-video-downloader.cask', :for => :osx
+dep '4k-video-downloader.cask', :for => :osx
