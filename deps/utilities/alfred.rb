1b7cbdffcbea032b54d084568ccd56b291061eca
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2015-03-30 00:38:40 +0200

    Add TODO for manual steps after installation

--- deps/utilities/alfred.rb
+++ deps/utilities/alfred.rb
@@ -5,3 +5,8 @@ end
 
 dep 'alfred.cask'
 dep 'com.runningwithcrayons.Alfred-2.accessibility_api'
+
+# TODO start the application and list what must be done in the settings
+# * start at login
+# * shortcut
+# * enable clipboard history
