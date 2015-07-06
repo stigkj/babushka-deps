a2bb038bf3b79358dea34229d8e23333ae59400c
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2015-07-06 09:50:10 +0200

    Improve requires for Tomcat 7

--- deps/development/tomcat7.rb
+++ deps/development/tomcat7.rb
@@ -1,10 +1,11 @@
 dep 'Tomcat 7' do
   requires {
-    on :osx, 'homebrew versions', 'tomcat7.managed'
+    on :osx, 'tomcat7.managed'
     otherwise { unsupported_platform! }
   }
 end
 
 dep 'tomcat7.managed', :for => :osx do
+  requires 'homebrew versions'
   provides 'catalina'
 end
