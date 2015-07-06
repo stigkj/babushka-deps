48519bc740af66cd5a6a29883f1ea8febaf6ba5c
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2015-07-06 09:48:49 +0200

    Setup go-lang with paths and environment variables

--- deps/development/go-lang.rb
+++ deps/development/go-lang.rb
@@ -1,12 +1,36 @@
 dep 'go-lang' do
   requires 'go.managed',
-           'Directories for GOPATH',
-           'Go environment variables'
+           'go.shell_config'
 
 end
 
 dep 'go.managed'
 
-# TODO Setup Go directories and environment variables
-dep 'Directories for GOPATH'
-dep 'Go environment variables'
+dep 'go.shell_config' do
+  requires 'Directory for GOPATH'
+
+  must_include 'GOROOT'
+  content_to_append go_shell_config
+end
+
+dep 'Directory for GOPATH' do
+  met? { go_path.exist? }
+  meet {
+    log_block 'Creating directory for GOPATH' do
+      go_path.p.mkdir
+    end
+  }
+end
+
+def go_shell_config
+  <<-EOF.unindent
+    # Inserted by go-lang dependency in Babushka
+    export GOROOT=/usr/local/opt/go/libexec
+    export GOPATH=#{go_path}
+    export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+  EOF
+end
+
+def go_path
+  "#{ENV['HOME']}/go".p
+end
