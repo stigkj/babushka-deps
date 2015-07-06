737f57166e7e1d873ec9c69473609f8da57e72e4
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2015-07-06 10:27:55 +0200

    Add Disk Drill, tool for recovering files

---
+++ deps/utilities/disk-drill.rb
@@ -0,0 +1,13 @@
+# Utility for recovering files
+#
+# Only have a license for 1.x, can upgrade through this link:
+# https://cleverfiles.cleverbridge.com/881/uurl-vujvgekmta
+#
+# More info in mail:
+# mailplane://stigkj%40gmail.com/#search/bundlehunt/142eb8abbec1ccc3
+# https://mail.google.com/mail/?pli=1#search/bundlehunt/142eb8abbec1ccc3
+#
+# Link for the old release:
+# http://www.cleverfiles.com/releases/old/DiskDrill_1.8.214.dmg
+
+dep 'disk-drill.cask', :for => :osx
