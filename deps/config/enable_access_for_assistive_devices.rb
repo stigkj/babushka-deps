45e4b2c1c032e44706e156b2c9c2c4778e54a2d3
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2015-07-06 08:59:33 +0200

    Make 'path' into a fancypath to enable path.exist?

--- deps/config/enable_access_for_assistive_devices.rb
+++ deps/config/enable_access_for_assistive_devices.rb
@@ -1,8 +1,8 @@
 dep 'Enable access for assistive devices', :for => :osx do
-  met? { File.exists? path }
+  met? { path.exist? }
   meet { log_shell 'Enabling access for assistive devices', "touch #{path}", :sudo => true }
 end
 
 def path
-  '/private/var/db/.AccessibilityAPIEnabled'
+  '/private/var/db/.AccessibilityAPIEnabled'.p
 end
