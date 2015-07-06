b392edf80fa157615fbc6832720fe0063fc7e7cc
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2015-07-06 09:54:38 +0200

    Add TODO for installing 1Password license

--- deps/utilities/1password.rb
+++ deps/utilities/1password.rb
@@ -6,4 +6,7 @@ dep '1Password' do
   requires_when_unmet 'spawn OS X app'.with('/opt/homebrew-cask/Caskroom/1password/*/1Password 5.app')
 end
 
+# TODO install license automatically?
+# Is located in ~/Dropbox/Software/Licences/
+
 dep '1password.cask', :for => :osx
