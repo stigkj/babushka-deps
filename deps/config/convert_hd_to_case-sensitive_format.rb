229323398d2acbc9621e35ed1315052164422c00
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2015-10-19 15:20:19 +0200

    Case-sensitivity is not important on VirtualBox

--- deps/config/convert_hd_to_case-sensitive_format.rb
+++ deps/config/convert_hd_to_case-sensitive_format.rb
@@ -1,6 +1,9 @@
 dep 'Convert HD to case-sensitive format', :for => :osx do
   requires_when_unmet 'iPartition.app'
 
-  met? { shell('diskutil info disk1').include? 'Case-sensitive' }
+  met? {
+    (shell('diskutil info disk1 || echo error').include? 'Case-sensitive') ||
+    (shell('diskutil info disk0 || echo error').include? 'VBOX')
+  }
   meet { Babushka::Prompt.confirm('Start iPartition and convert partition to case-sensitive format before going any further', :default => 'y') }
 end
