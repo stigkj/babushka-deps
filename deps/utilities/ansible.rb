6eb8f08b34591e470edc39cc188fbd18a538008c
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2015-11-02 14:55:48 +0100

    Add ansible-shell to standard Ansible installation

--- deps/utilities/ansible.rb
+++ deps/utilities/ansible.rb
@@ -1,2 +1,11 @@
 # IT automation simplified
+dep 'Ansible' do
+  requires 'ansible.managed',
+           'ansible-shell.pip'
+end
+
 dep 'ansible.managed'
+
+dep 'ansible-shell.pip' do
+  requires 'ansible.managed'
+end
