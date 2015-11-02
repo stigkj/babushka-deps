2742027cde21523b9309a4be880909c32eaa314d
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2015-11-02 14:52:35 +0100

    Add TODO for setting env.vars for GUI apps

---
+++ deps/config/set_env_vars_for_gui_apps.rb
@@ -0,0 +1,8 @@
+# TODO
+# look at these:
+#
+# http://stackoverflow.com/questions/25385934/setting-environment-variables-via-launchd-conf-no-longer-works-in-os-x-yosemite/26477515#26477515
+# http://apple.stackexchange.com/questions/153402/in-osx-yosemite-why-can-i-set-many-environment-variables-for-gui-apps-but-cann
+# http://apple.stackexchange.com/questions/152294/shell-environment-variables-not-available-in-gui-apps-even-if-app-launched-via-c?lq=1
+# http://neu242.livejournal.com/54945.html
+# http://www.openradar.me/18945659
