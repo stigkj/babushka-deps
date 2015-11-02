754fad556930426753a71bbdcc8317805610650e
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2015-11-02 14:36:19 +0100

    Method for path must be private to dependency
    As path is defined elsewhere, this would sometimes override that one
    and crashing the run.

--- deps/config/enable_access_for_assistive_devices.rb
+++ deps/config/enable_access_for_assistive_devices.rb
@@ -1,8 +1,8 @@
 dep 'Enable access for assistive devices', :for => :osx do
   met? { path.exist? }
   meet { log_shell 'Enabling access for assistive devices', "touch #{path}", :sudo => true }
-end
 
-def path
-  '/private/var/db/.AccessibilityAPIEnabled'.p
+  def path
+    '/private/var/db/.AccessibilityAPIEnabled'.p
+  end
 end
