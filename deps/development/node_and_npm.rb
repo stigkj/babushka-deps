fafe69e3b86a19ee8acf01017e81766bcbf55ce7
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2015-11-02 14:51:41 +0100

    Link node & npm into system path for GUI apps

--- deps/development/node_and_npm.rb
+++ deps/development/node_and_npm.rb
@@ -1,6 +1,30 @@
 dep 'node & npm' do
-  requires 'node.managed'
+  requires 'node.managed',
+           'link node & npm into system path to work from GUI apps'
 end
 
 dep 'node.managed'
 
+# This is needed as PATH is not set properly in GUI apps, only a few of them, like /usr/bin
+dep 'link node & npm into system path to work from GUI apps' do
+  met? { binaries.all? { |binary| "#{bin_path}/#{binary}".p.exists? } }
+  meet {
+    log_block "Linking Node command line utilities #{binaries} into #{bin_path}" do
+      binaries.each { |binary|
+        shell %Q{ln -sf "#{path_to_binaries}/#{binary}" "#{bin_path}/#{binary}"}, :sudo => true
+      }
+    end
+  }
+
+  def binaries
+    %w[node npm]
+  end
+
+  def bin_path
+    '/usr/bin'.p
+  end
+
+  def path_to_binaries
+    '/usr/local/bin'.p
+  end
+end
