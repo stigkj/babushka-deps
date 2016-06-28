ec3c467307131f81a5c011aac78af9aa4853aca1
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2016-06-28 12:42:06 +0200

    Use v3.5.1 of iPartition

--- deps/utilities/ipartition.rb
+++ deps/utilities/ipartition.rb
@@ -1,3 +1,3 @@
 dep 'iPartition.app', :for => :osx do
-  source "file://#{ENV['HOME']}/Dropbox/Software/iPartition-3.4.5.zip"
+  source 'https://coriolis-systems.com/downloads/iPartition-3.5.1.dmg'
 end
