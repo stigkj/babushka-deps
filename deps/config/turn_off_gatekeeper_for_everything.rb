667476ae8a4fa7f125b8e3aaef63786aa7beab0a
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2016-08-12 17:41:34 +0200

    Turn off automatic re-enabling of Gatekeeper
    If this option is not turned off, Gatekeeper will be re-enabled after 30
    days automatically.

--- deps/config/turn_off_gatekeeper_for_everything.rb
+++ deps/config/turn_off_gatekeeper_for_everything.rb
@@ -1,4 +1,16 @@
+dep 'Turn off Gatekeeper for everything' do
+  requires 'Turn off Gatekeeper for all applications',
+           'Turn off automatic re-enabling of Gatekeeper after 30 days.osx_defaults'
+end
+
 dep 'Turn off Gatekeeper for all applications' do
   met? { !shell? 'spctl --status' }
   meet { log_shell 'Turning off Gatekeeper for all applications', "spctl --master-disable", :sudo => true }
+end
+
+dep 'Turn off automatic re-enabling of Gatekeeper after 30 days.osx_defaults' do
+  domain 'NSGlobalDomain'
+  key 'GKAutoRearm'
+  type 'bool'
+  value 'false'
 end
