1b7fca614865dcf89b8cb34f7d5b30f22aef82df
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2016-08-29 15:03:15 +0200

    Change to use Docker.app
    Comment out all the old stuff

--- deps/utilities/docker.rb
+++ deps/utilities/docker.rb
@@ -7,30 +7,30 @@ dep 'docker.cask'
 # OLD STUFF
 
 # Easily manage containers
-dep 'docker.managed'
+#dep 'docker.managed'
 
 # Create and manage Docker servers locally or in the cloud
-dep 'docker-machine.managed' do
-  requires 'docker-machine.shell_config'
-end
+#dep 'docker-machine.managed' do
+#  requires 'docker-machine.shell_config'
+#end
 
 # Manage multi-container applications with a single file
-dep 'docker-compose.managed'
+#dep 'docker-compose.managed'
 
 # A Docker-native clustering system
-dep 'docker-swarm.managed'
+#dep 'docker-swarm.managed'
 
-dep 'docker-machine.shell_config' do
-  must_include 'DOCKER_HOST'
-  content_to_append docker_machine_shell_config
-end
+#dep 'docker-machine.shell_config' do
+#  must_include 'DOCKER_HOST'
+#  content_to_append docker_machine_shell_config
+#end
 
-def docker_machine_shell_config
-  <<-EOF.unindent
-    # Inserted by Docker dependency in Babushka
-    export DOCKER_TLS_VERIFY="1"
-    export DOCKER_HOST="tcp://192.168.99.100:2376"
-    export DOCKER_CERT_PATH="/Users/stiklepp/.docker/machine/machines/dev"
-    export DOCKER_MACHINE_NAME="dev"
-  EOF
-end
+#def docker_machine_shell_config
+#  <<-EOF.unindent
+#    # Inserted by Docker dependency in Babushka
+#    export DOCKER_TLS_VERIFY="1"
+#    export DOCKER_HOST="tcp://192.168.99.100:2376"
+#    export DOCKER_CERT_PATH="/Users/stiklepp/.docker/machine/machines/dev"
+#    export DOCKER_MACHINE_NAME="dev"
+#  EOF
+#end
