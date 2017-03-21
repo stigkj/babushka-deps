356271a305ff3c99dbfd71aaf361581a96c48b5d
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2017-03-21 10:55:57 +0100

    Add yarn in node dep
    Also put binaries installed through yarn in PATH.

--- deps/development/node_and_npm.rb
+++ deps/development/node_and_npm.rb
@@ -1,12 +1,35 @@
 dep 'node & npm' do
   requires 'node.managed',
-           'link node & npm into system path to work from GUI apps'
+           'yarn',
+           'link node binaries into system path to work from GUI apps'
 end
 
 dep 'node.managed'
 
+dep 'yarn' do
+  requires 'yarn.managed',
+           'yarn.shell_config'
+end
+
+dep 'yarn.managed'
+dep 'yarn.shell_config' do
+  must_include yarn_deps_bin_path
+  content_to_append yarn_shell_config
+end
+
+def yarn_shell_config
+  <<-EOF.unindent
+    # Inserted by yarn dependency in Babushka
+    export PATH=$PATH:#{yarn_deps_bin_path}
+  EOF
+end
+
+def yarn_deps_bin_path
+  '~/.config/yarn/global/node_modules/.bin'.p
+end
+
 # This is needed as PATH is not set properly in GUI apps, only a few of them, like /usr/bin
-dep 'link node & npm into system path to work from GUI apps' do
+dep 'link node binaries into system path to work from GUI apps' do
   met? { binaries.all? { |binary| "#{bin_path}/#{binary}".p.exists? } }
   meet {
     log_block "Linking Node command line utilities #{binaries} into #{bin_path}" do
@@ -17,7 +40,7 @@ dep 'link node & npm into system path to work from GUI apps' do
   }
 
   def binaries
-    %w[node npm]
+    %w[node npm yarn]
   end
 
   def bin_path
