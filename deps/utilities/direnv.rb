e3355056c09c4a860b30f3d05df8be068996fadd
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2017-04-20 11:46:10 +0200

    Add direnv

---
+++ deps/utilities/direnv.rb
@@ -0,0 +1,20 @@
+# Environment switcher for the shell
+dep 'direnv' do
+  requires 'direnv.managed',
+           'direnv.shell_config'
+end
+
+dep 'direnv.managed'
+
+dep 'direnv.shell_config' do
+  must_include 'direnv'
+  content_to_append direnv_shell_config
+end
+
+def direnv_shell_config
+  <<-EOF.unindent
+    # Inserted by direnv dependency in Babushka
+    eval "$(direnv hook zsh)"
+  EOF
+end
+
