e6c4fcdf6baa2992376fe2e2d0dd5f3df087ad0d
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2017-04-24 18:16:41 +0200

    Add utilities for zsh

--- deps/utilities/zsh.rb
+++ deps/utilities/zsh.rb
@@ -1,1 +1,15 @@
 dep 'zsh.managed'
+
+# TODO look at this, Fish-like suggestions
+dep 'zsh-autosuggestions.managed'
+
+# TODO choose zsh manager, remove prezto
+dep 'zplug.managed'
+dep 'zgen.managed'
+dep 'antigen.managed'
+
+# TODO look at these plugins too
+dep 'zaw.managed' # https://github.com/zsh-users/zaw
+dep 'fz.managed'  # https://github.com/changyuheng/fz
+
+# TODO https://github.com/djui/alias-tips
