49af591d6a0ec1b90aaf03f60dc8d1596aa628c3
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2017-09-20 13:39:11 +0200

    Add TODO for looking at fasd alternatives

--- deps/utilities/fasd.rb
+++ deps/utilities/fasd.rb
@@ -1,2 +1,5 @@
 # A command-line productivity booster with quick access to files/folders
+# TODO use https://github.com/b4b4r07/enhancd instead
+# look at
+# https://github.com/jlevy/the-art-of-command-line/issues/88
 dep 'fasd.managed'
