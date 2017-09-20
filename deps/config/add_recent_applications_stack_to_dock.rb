86d7e1a9abf3ea39afb22144e9caebe20a3dc838
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2017-09-20 13:24:18 +0200

    Add TODO for adding recent applications to dock

---
+++ deps/config/add_recent_applications_stack_to_dock.rb
@@ -0,0 +1,4 @@
+# TODO
+
+#Add a 'Recent Applications' stack to the Dock.
+#$ defaults write com.apple.dock persistent-others -array-add '{ "tile-data" = { "list-type" = 1; }; "tile-type" = "recents-tile"; }'
