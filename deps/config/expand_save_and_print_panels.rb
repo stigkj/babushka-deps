ce110a1b3659c5cfd51dd03fa2e210dccb80dcd8
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2017-09-20 13:24:55 +0200

    Add TODO for always extending save/print dialogs

---
+++ deps/config/expand_save_and_print_panels.rb
@@ -0,0 +1,7 @@
+# TODO
+
+#defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+#defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+
+#defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+#defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
