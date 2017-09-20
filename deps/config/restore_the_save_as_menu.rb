87578383a7d4d00741e38f46be5711574e3ea049
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2017-09-20 13:26:37 +0200

    Add TODO for restoring 'Save As...' menu item

---
+++ deps/config/restore_the_save_as_menu.rb
@@ -0,0 +1,4 @@
+# TODO
+
+#Restore the the 'Save As' menu item (Equivalent to adding a Keyboard shortcut in the System Preferences.app )
+#$ defaults write -g NSUserKeyEquivalents -dict-add 'Save As...' '@$S'
