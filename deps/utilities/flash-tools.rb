453c19bb5af901a4afdc8c127ea9f8a82d38bd7e
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2017-11-09 23:20:06 +0100

    Add flash-tools - scripts for flashing SD card images

---
+++ deps/utilities/flash-tools.rb
@@ -0,0 +1,13 @@
+# Several tools for flashing SD cards
+# TODO: choose one
+
+# A script to flash SD card images of any kind
+#
+# TODO: Make a dep of this:
+#
+# curl -O https://raw.githubusercontent.com/hypriot/flash/master/$(uname -s)/flash
+# chmod +x flash
+# sudo mv flash /usr/local/bin/flash
+
+# Burn images to SD cards or other flash based memory nice and easy
+dep 'fwup.managed'
