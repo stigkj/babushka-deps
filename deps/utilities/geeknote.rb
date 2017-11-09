7838497e521e00b43f25c42a1692c9bdedd12cc3
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2017-11-09 23:23:24 +0100

    Update description for geeknote

--- deps/utilities/geeknote.rb
+++ deps/utilities/geeknote.rb
@@ -1,1 +1,4 @@
-# TODO geeknote.me
+# Console client for Evernote
+# See https://github.com/jeffkowalski/geeknote
+# brew install --HEAD https://raw.githubusercontent.com/jeffkowalski/geeknote/master/geeknote.rb
+dep 'geeknote.managed'
