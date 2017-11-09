ef37b2c9fabd9038fbbb9456f3757418f0dcb793
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2017-11-09 23:31:36 +0100

    Add gtm - simple time tracking for git projects

---
+++ deps/utilities/gtm.rb
@@ -0,0 +1,5 @@
+# Simple time tracking for git projects
+# TODO brew tap git-time-metric/gtm
+# TODO look at https://github.com/git-time-metric/gtm-terminal-plugin
+# TODO others too: https://github.com/git-time-metric/
+dep 'gtm.managed'
