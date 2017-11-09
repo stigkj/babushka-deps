eafc525afbb92d08906e92379a2df5ae9be71da1
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2017-11-09 16:17:03 +0100

    Add local web server for development
    Which to choose? For now devd, but maybe local-web-server is better.
    Or maybe have several of these as they are partly complimentary.

---
+++ deps/network/local-web-server.rb
@@ -0,0 +1,5 @@
+# TODO need more than devd?
+# * slinky (https://github.com/mwylde/slinky)
+# * reloadify (https://github.com/alessioalex/reloadify)
+# * local-web-server (https://github.com/lwsjs/local-web-server)
+dep 'devd.managed'
