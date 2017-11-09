b4dd432eb56c4d6e4b580f6ebe3e715f094c4c99
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2017-11-10 00:06:06 +0100

    Add zookeeper tools

---
+++ deps/utilities/zookeeper-tools.rb
@@ -0,0 +1,26 @@
+# Some zookeeper tools
+
+# Nice cli for ZooKeeper; installed with go get
+dep 'zkcli.go'
+
+# A cli and scriptable shell
+# https://github.com/rgs1/zk_shell
+dep 'zk-shell.pip'
+
+# A nice interactive cli
+# https://github.com/mpetazzoni/kazoocli
+dep 'kazoocli.pip'
+
+# ^^ these two must have a matching version of readline (6.x) installed
+
+# Check the status of SolrCloud by looking at its ZK data
+# https://github.com/bendemott/solr-zkutil
+dep 'solr-zkutil'
+
+# Admin console in JavaEE
+# https://github.com/luoyan35714/zookeeper_admin
+dep 'zookeeper_admin'
+
+# Traffic decoder/analyzer
+# https://github.com/jeffbean/zkpacket
+dep 'zkpacket'
