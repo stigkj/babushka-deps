7ae3d9fab0b8bd4af594d505985d69c3d644351e
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2017-11-09 23:39:49 +0100

    Add log-shell-history - tools for logging shell history

---
+++ deps/utilities/log-shell-history.rb
@@ -0,0 +1,13 @@
+# TODO Decide on one of these for logging shell history
+
+# https://github.com/autochthe/history
+# https://github.com/tymm/zsh-directory-history
+# https://github.com/jcsalterego/historian
+# https://github.com/thenewwazoo/bash-history-sqlite
+# https://github.com/joshuacronemeyer/shellsink
+# https://github.com/andmarios/bashistdb
+
+# Looks like this is the best
+# https://github.com/barabo/advanced-shell-history
+
+dep 'his.managed'
