cb3d82f0394dc0bcb6e3eecd066cee8527ba2f19
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2017-11-09 22:41:13 +0100

    Add convertio - cli that converts div file formats
    This is done through an online api.

---
+++ deps/utilities/convertio.rb
@@ -0,0 +1,6 @@
+# A cli for converting doc, etc through https://convertio.co/cli/
+
+# $ wget --no-check-certificate -O '/usr/local/bin/convertio' https://api.convertio.co/convertio
+# $ chmod +x /usr/local/bin/convertio
+
+# export CONVERTIO_API_KEY=62a782f9f9e6407f4c09c30b9745ba6b
