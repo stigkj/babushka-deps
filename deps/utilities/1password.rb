b262c2251df767e5652d45358361c3a824a9fdcc
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2017-11-14 15:00:45 +0100

    Fix launching of 1Password after installation

--- deps/utilities/1password.rb
+++ deps/utilities/1password.rb
@@ -3,10 +3,13 @@ dep '1Password' do
     on :osx, '1password.cask'
     otherwise { unsupported_platform! }
   }
-  requires_when_unmet 'spawn OS X app'.with('/opt/homebrew-cask/Caskroom/1password/*/1Password 5.app')
 end
 
 # TODO install license automatically?
-# Is located in ~/Dropbox/Software/Licences/
+# It is located in ~/Dropbox/Software/Licences/
 
-dep '1password.cask', :for => :osx
+dep '1password.cask', :for => :osx do
+  after {
+    Process.spawn "open /Applications/1Password*.app"
+  }
+end
