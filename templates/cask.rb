9a36d15288af984fd17628213e2dcb5c8ddba4fc
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2017-11-14 15:08:06 +0100

    Merge cask helper code into one file

--- templates/cask.rb
+++ templates/cask.rb
@@ -12,3 +12,37 @@ meta :cask do
     meet { Babushka::CaskHelper.handle_install! installs }
   }
 end
+
+# A PkgHelper for managing Homebrew Cask packages
+module Babushka
+  class CaskHelper < BrewHelper
+  class << self
+    def pkg_type; :cask end
+    def pkg_binary; 'brew' end
+    def pkg_cmd; 'brew cask' end
+    def manager_key; :cask end
+
+    # Delegate prefix to Homebrew helper
+    def s_prefix
+      Babushka::BrewHelper.prefix
+    end
+
+    def has_pkg? pkg
+      pkg_name = pkg.respond_to?(:name) ? pkg.name : pkg
+      !Babushka::ShellHelpers.shell("brew cask info #{pkg_name}").include? 'Not installed'
+    end
+
+    # With new Homebrew releases which includes cask, 'brew update' must be used instead of 'brew cask update'
+    def pkg_update_command
+      "#{pkg_binary} update"
+    end
+
+    # Homebrew Cask packages get installed in the "caskroom" which is "<prefix>/Caskroom"
+    def installed_pkgs_path
+      "/usr/local/Caskroom".p
+    end
+  end
+  end
+end
+
+Babushka::PkgHelper.all_manager_keys << Babushka::CaskHelper.manager_key
