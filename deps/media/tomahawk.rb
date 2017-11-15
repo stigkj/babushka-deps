7e3311b07826643f3f53b5a38e3f05e79ee75955
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2017-11-15 16:32:02 +0100

    Fix Tomahawk dep
    Just download its installer from the web site.

--- deps/media/tomahawk.rb
+++ deps/media/tomahawk.rb
@@ -1,9 +1,13 @@
+# Plays music from a lot of sources, like Spotify, Google Play, Tidal, etc.
+# https://www.tomahawk-player.org
 dep 'Tomahawk' do
   requires {
-    on :osx, 'tomahawk.cask'
+    on :osx, 'tomahawk.installer'
     # TODO setup for Linux too
     otherwise { unsupported_platform! }
   }
 end
 
-dep 'tomahawk.cask', :for => :osx
+dep 'tomahawk.installer', :for => :osx do
+  source 'http://download.tomahawk-player.org/Tomahawk-0.8.4.dmg'
+end
