31d37185b5c43ad5205317cbb2c8b6693658e1e1
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2017-11-15 16:22:31 +0100

    Move REST clients into one dep

--- deps/network/insomnia.rb
+++
@@ -1,3 +0,0 @@
-# An open source GUI REST client
-# https://insomnia.rest
-dep 'insomnia.managed'
--- deps/network/rest-client.rb
+++ deps/network/rest-client.rb
@@ -1,3 +1,8 @@
+dep 'REST client' do
+  requires 'Paw',
+           'insomnia.cask'
+end
+
 # Nice REST client
 dep 'Paw' do
   requires 'paw.cask',
@@ -9,3 +14,7 @@ dep 'paw.cask', :for => :osx
 dep 'install license for Paw' do
     # TODO read from license.txt and install it of possible. If not, just show it.
 end
+
+# An open source GUI REST client
+# https://insomnia.rest
+dep 'insomnia.cask'
