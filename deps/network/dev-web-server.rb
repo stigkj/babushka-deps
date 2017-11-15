2db09c987ad740939ee52327ee26a0e8e98a7817
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2017-11-15 16:35:10 +0100

    Let devd be the dev web server to use
    Also improve name of dep.

--- deps/network/dev-web-server.rb
+++ deps/network/dev-web-server.rb
@@ -1,3 +1,7 @@
+dep 'dev web server' do
+  requires 'devd.managed'
+end
+
 # TODO need more than devd?
 # * slinky (https://github.com/mwylde/slinky)
 # * reloadify (https://github.com/alessioalex/reloadify)
