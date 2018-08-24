ff17da96c6dd59974b8f646db630ec6320894267
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2018-08-24 21:37:39 +0100

    Add TODO for having a logout/reboot in the script

--- deps/config/use_function_keys_as_function_keys.rb
+++ deps/config/use_function_keys_as_function_keys.rb
@@ -1,3 +1,4 @@
+# TODO seems one must logout or reboot for this to take effect
 dep 'Use function keys as function keys.osx_defaults' do
   domain 'NSGlobalDomain'
   key 'com.apple.keyboard.fnState'
