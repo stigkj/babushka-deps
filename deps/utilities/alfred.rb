3c0357ef4a7394cec54d0c7f993c912d299ae1fb
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2018-08-27 09:09:21 +0200

    No need for setup of accessibility for Alfred
    This is moved into the cask now.

--- deps/utilities/alfred.rb
+++ deps/utilities/alfred.rb
@@ -1,10 +1,8 @@
 dep 'Alfred', :for => :osx do
-  requires 'alfred.cask',
-           'com.runningwithcrayons.Alfred-2.accessibility_api'
+  requires 'alfred.cask'
 end
 
 dep 'alfred.cask'
-dep 'com.runningwithcrayons.Alfred-2.accessibility_api'
 
 # TODO start the application and list what must be done in the settings
 # * start at login
