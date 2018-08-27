34f49454e2f45b22adf54243b0e9964f06aac545
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2018-08-27 08:41:03 +0200

    Need to tell Babushka the name of cli for httpie
    This is needed because the cli does not have the same name as the
    dependency.

--- deps/network/http-cli.rb
+++ deps/network/http-cli.rb
@@ -4,7 +4,9 @@ dep 'http cli' do
 end
 
 # A simple-to-use HTTP command line client
-dep 'httpie.managed'
+dep 'httpie.managed' do
+  provides 'http'
+end
 
 # A simple-to-use interactive HTTP command line client
 dep 'http-prompt.pip'
