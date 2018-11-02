a767a2717379e226e9a80a66357cd44883fd431b
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2018-11-02 16:44:40 +0100

    Add fd and change to use ripgrep in search tools
    fd is an alternative to find and ripgrep replaces sift.

--- deps/utilities/search_tools.rb
+++ deps/utilities/search_tools.rb
@@ -1,15 +1,20 @@
-dep 'Search tool' do
-  requires 'sift.managed'
+dep 'Search tools' do
+  requires 'ripgrep.managed',
+	       'fd.managed'
 end
+
+
+dep 'ripgrep.managed' do
+  provides 'rg'
+end
+
+# A simple, fast and user-friendly alternative to 'find'
+dep 'fd.managed'
 
 # TODO maybe setup saner defaults in global config?
 dep 'sift.managed'
 
 dep 'ucg.managed'
-
-dep 'ripgrep.managed' do
-  provides 'rg'
-end
 
 dep 'the_silver_searcher.managed' do
   provides 'ag'
