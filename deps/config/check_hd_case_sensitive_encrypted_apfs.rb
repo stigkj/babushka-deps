472b824ed50e633f6a3db548322df63188f156b3
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2018-11-02 16:53:46 +0100

    Instead of converting, check HD has correct format
    That is, make sure the main disk has case-sensitive encrypted APFS.

---
+++ deps/config/check_hd_case_sensitive_encrypted_apfs.rb
@@ -0,0 +1,27 @@
+dep 'Check HD is in case-sensitive encrypted APFS format' do
+  met? {
+    (casesensitive_apfs_check_cache_file.exists? || is_casesensitive_apfs) &&
+        (hd_encrypted_check_cache_file.exists? || is_encrypted)
+  }
+  meet {
+    Babushka::Prompt.confirm('Format disk in case-sensitive encrypted APFS', :default => 'y')
+  }
+end
+
+def is_casesensitive_apfs
+  shell('diskutil info / || echo error') =~ /File System Personality:.*Case-sensitive APFS/ &&
+      casesensitive_apfs_check_cache_file.touch
+end
+
+def is_encrypted
+  shell('diskutil apfs list || echo error') =~ /FileVault:\s*Yes/ &&
+      hd_encrypted_check_cache_file.touch
+end
+
+def casesensitive_apfs_check_cache_file
+  '/tmp/casesensitive_apfs_check_passed'.p
+end
+
+def hd_encrypted_check_cache_file
+  '/tmp/hd_encrypted_check_passed'.p
+end
--- deps/config/convert_hd_to_case-sensitive_format.rb
+++
@@ -1,9 +0,0 @@
-dep 'Convert HD to case-sensitive format', :for => :osx do
-  requires_when_unmet 'iPartition.app'
-
-  met? {
-    (shell('diskutil info disk1 || echo error').include? 'Case-sensitive') ||
-    (shell('diskutil info disk0 || echo error').include? 'VBOX')
-  }
-  meet { Babushka::Prompt.confirm('Start iPartition and convert partition to case-sensitive format before going any further', :default => 'y') }
-end
