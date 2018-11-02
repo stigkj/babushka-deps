997fcf4138411e6f5c5d27f36147f839744aed0f
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2018-11-02 15:59:34 +0100

    Change shell_config to only append to .zshrc
    bash is not used, so no need to complicate things.

--- helpers/shell_config.rb
+++ helpers/shell_config.rb
@@ -1,3 +1,4 @@
+# Appends config for an application to zsh profile file
 meta :shell_config do
   accepts_list_for :must_include
   accepts_value_for :content_to_append
@@ -5,24 +6,20 @@ meta :shell_config do
 
   template {
     met? {
-      shell_profile_files.all? { |pathname|
-        pathname.exists? &&
+      zsh_profile.exists? &&
         must_include.all? { |to_include|
-          pathname.read.include?(to_include)
+          zsh_profile.read.include? to_include
         }
-      }
     }
     meet {
-      log_block "Install #{application} into shell profile files ['~/.bashrc', '~/.zshrc']" do
-        shell_profile_files.each { |path|
-          path.append content_to_append
-        }
+      log_block "Appends #{application} config to zsh profile file '~/.zshrc'" do
+        zsh_profile.append "\n#{content_to_append}"
       end
     }
   }
 
-  def shell_profile_files
-    ['~/.bashrc'.p, '~/.zshrc'.p]
+  def zsh_profile
+    '~/.zshrc'.p
   end
 end
 
