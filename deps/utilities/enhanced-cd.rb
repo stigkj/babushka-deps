3d3f7bc3d23bbfab4a5204fa8c831ff8f8e1789f
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2018-11-02 16:32:03 +0100

    Use enhancd instead of fasd

---
+++ deps/utilities/enhanced-cd.rb
@@ -0,0 +1,14 @@
+dep 'Enhanced cd' do
+  requires 'enhancd.zplug_plugin'
+end
+
+# Next generation cd command with interactive filter
+dep 'enhancd.zplug_plugin' do
+  requires 'fzy.managed'
+
+  name 'b4b4r07/enhancd'
+  tags 'use:init.sh'
+end
+
+# TODO look at including fasd too which has some more features
+dep 'fasd.managed'
--- deps/utilities/fasd.rb
+++
@@ -1,5 +0,0 @@
-# A command-line productivity booster with quick access to files/folders
-# TODO use https://github.com/b4b4r07/enhancd instead
-# look at
-# https://github.com/jlevy/the-art-of-command-line/issues/88
-dep 'fasd.managed'
