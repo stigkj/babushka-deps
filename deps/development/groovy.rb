c81e4f7a2232abd4853f64a465b69e2c05ab2a33
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2018-11-04 15:47:35 +0100

    Homebrew needs java installed to install groovy

--- deps/development/groovy.rb
+++ deps/development/groovy.rb
@@ -1,5 +1,9 @@
 dep 'Groovy' do
-  requires 'groovy.managed'
+  requires 'java.managed',
+           'groovy.managed'
 end
 
+# Homebrew needs to have java installed before it can install groovy
+# TODO create a asdf plugin instead
+dep 'java.managed'
 dep 'groovy.managed'
