e5109576a0d694bb3ae7ccbabfae20023ce707ff
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2018-11-04 15:14:22 +0100

    Must create ~/.config for new mackup to work
    Mackup needs the XDG config directory to be present for restore to work.

--- deps/utilities/mackup.rb
+++ deps/utilities/mackup.rb
@@ -1,15 +1,34 @@
 dep 'mackup' do
   requires 'Dropbox',
            'Install mackup with easy_install',
+           'Create the XDG .config directory',
            'Mackup restore must run before continuing'
 end
 
 dep 'Install mackup with easy_install' do
   met? { in_path? 'mackup' }
-  meet { log_shell 'Installing mackup', 'easy_install mackup', :spinner => true, :sudo => true }
+  meet {
+    log_shell 'Installing mackup', 'easy_install mackup', :spinner => true, :sudo => true
+  }
+end
+
+# v0.9.2 of mackup needs the XDG config directory to be present before restore
+dep 'Create the XDG .config directory' do
+  met? { xdg_config_directory.exists? }
+  meet {
+    log_block "Creating #{xdg_config_directory}" do
+      xdg_config_directory.mkdir
+    end
+  }
+
+  def xdg_config_directory
+    '~/.config'.p
+  end
 end
 
 dep 'Mackup restore must run before continuing' do
   met? { '~/.git_global'.p.symlink? }
-  meet { Babushka::Prompt.confirm('Run mackup restore now in another shell before pressing enter', :default => 'y') }
+  meet {
+    confirm('Run mackup restore now in another shell before pressing enter', :default => 'y')
+  }
 end
