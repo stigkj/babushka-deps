1380a322d1eecee92951fa04907a1781193d2ecb
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2018-11-05 10:09:15 +0100

    Must tap a Homebrew repo to install ghrls

--- deps/development/github-tools.rb
+++ deps/development/github-tools.rb
@@ -1,13 +1,16 @@
 dep 'GitHub tools' do
   requires 'ghrepo.npm',
-           'ghrls.managed'
+           'ghrls'
 end
 
 # Create GitHub repo for current folder
 dep 'ghrepo.npm'
 
 # Lists and describes github releases
-# TODO: install from tap --> https://github.com/dtan4/ghrls
+dep 'ghrls' do
+  requires 'homebrew tap'.with('dtan4/tools'),
+           'ghrls.managed'
+end
 dep 'ghrls.managed'
 
 # Writes the README content of a GitHub repository to stdout
