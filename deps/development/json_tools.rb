6f9c9fbc93ac8a4f28209854e699d0c76f9931c7
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2019-01-02 10:03:01 +0100

    Add some more json tools

--- deps/development/json_tools.rb
+++ deps/development/json_tools.rb
@@ -1,7 +1,8 @@
 dep 'JSON tools' do
   requires 'jq.managed',
            'jid.managed',
-           'gron.managed'
+           'gron.managed',
+           'jl'
 end
 
 # A lightweight and flexibl e command-line JSON processor
@@ -12,6 +13,13 @@ dep 'jid.managed'
 
 # "Flattens" json to make it easily searchable with grep, etc.
 dep 'gron.managed'
+
+# Handles structured json logs
+dep 'jl' do
+  requires 'homebrew tap'.with('koenbollen/public'),
+           'jl.managed'
+end
+dep 'jl.managed'
 
 # TODO install xml2json converter: https://github.com/hidu/xml2json
 dep 'xml2json.go'
