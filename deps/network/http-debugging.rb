65d2eeab2bf05459eb8c21bbd05eb5eaa3ff8e2e
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2019-01-03 16:12:33 +0100

    Add Charles beta

---
+++ deps/network/http-debugging.rb
@@ -0,0 +1,13 @@
+# HTTP debugging and packet capture & analysis utilities (aka tcpdump for HTTP)
+dep 'http packet capture' do
+  requires 'httpflow.managed',
+           'charles-beta.cask'
+end
+
+# Think this is the best one
+dep 'httpflow.managed'
+
+dep 'charles-beta.cask'
+
+dep 'httpry.managed'
+
--- deps/network/http-packet-capture.rb
+++
@@ -1,9 +0,0 @@
-# Packet capture and analysis utility similar to tcpdump for HTTP
-dep 'http packet capture' do
-  requires 'httpflow.managed'
-end
-
-# Think this is the best one
-dep 'httpflow.managed'
-
-dep 'httpry.managed'
