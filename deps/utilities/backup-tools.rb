63bfa4d9281955e5368f53355e873b19bf73200c
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2019-08-28 15:35:27 +0200

    Add TODO for looking at backup tools

---
+++ deps/utilities/backup-tools.rb
@@ -0,0 +1,8 @@
+# TODO look at
+# https://duplicacy.com
+# https://www.duplicati.com
+# https://relicabackup.com
+# https://www.retrospect.com/en/products/solo
+# http://duplicity.nongnu.org
+# http://www.hashbackup.com
+# 
