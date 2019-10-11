aaf20056f4d6035ffa3b0eaa2858165fb637df6a
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2019-10-11 11:30:14 +0200

    Add different gui/shell tools for Docker
    These need to be setup properly.

--- deps/development/docker-tools.rb
+++ deps/development/docker-tools.rb
@@ -40,5 +40,23 @@ dep 'skopeo.go'
 # Run Docker images as native commands
 dep 'whalebrew.managed'
 
+# A easier, more visual way to interact with Docker daemon
+# TODO setup correctly
+dep 'supdock.npm'
+
+# A shell for Docker with autocompletion and syntax highligthing
+# TODO setup correctly
+dep 'wharfee.pip'
+
+# A console gui for managing Docker containers and services
+# TODO setup correctly
+dep 'dockly.npm'
+
+# A console gui for exploring the layers of a Docker image
+# TODO setup installation correctly, see https://github.com/wagoodman/dive
+dep 'dive.go'
+
+# TODO look at https://github.com/shyiko/dockry
+
 # TODO create a docker plugin for asdf, for versioning of docker clients, or use https://github.com/howtowhale/dvm
 # TODO look at http://dockersl.im - minify and secure your Docker containers
