0ee06124462a253e181e12af0c2f71e44b568c69
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2019-10-13 11:15:47 +0200

    Add is-website-vulnerable cli
    This finds publicly known security vulnerabilities in a website's
    frontend JavaScript libraries.

---
+++ deps/development/is-website-vulnerable.rb
@@ -0,0 +1,6 @@
+# Finds publicly known security vulnerabilities in a website's frontend JavaScript libraries
+dep 'is-website-vulnerable' do
+  requires 'is-website-vulnerable.npm'
+end
+
+dep 'is-website-vulnerable.npm'
