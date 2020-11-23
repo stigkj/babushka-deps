64ab93d49c0f00ae701f4399af81c076b8fbd8d6
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2020-11-23 14:15:52 +0100

    Remove expired API key
    The API key should not be checked in anyway.

--- deps/utilities/convertio.rb
+++ deps/utilities/convertio.rb
@@ -3,4 +3,4 @@
 # $ wget --no-check-certificate -O '/usr/local/bin/convertio' https://api.convertio.co/convertio
 # $ chmod +x /usr/local/bin/convertio
 
-# export CONVERTIO_API_KEY=62a782f9f9e6407f4c09c30b9745ba6b
+# export CONVERTIO_API_KEY=<only add API key locally>
