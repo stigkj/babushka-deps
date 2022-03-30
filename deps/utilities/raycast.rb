5652a1a3e3d302f4fd41e3b6573dcffa4730a59f
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2022-03-30 09:43:03 +0200

    Add Raycast

---
+++ deps/utilities/raycast.rb
@@ -0,0 +1,10 @@
+dep 'Raycast', :for => :osx do
+  requires 'raycast.cask'
+end
+
+dep 'raycast.cask'
+
+# TODO start the application and list what must be done in the settings
+# * start at login
+# * shortcut
+# * enable clipboard history
