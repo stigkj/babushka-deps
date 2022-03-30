13c91a7cf625d3e3e9add0602a53c62336edc796
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2022-03-30 09:59:15 +0200

    Disable dep for imageoptim-cli
    This does not build on new Apple silicon.

--- deps/media/image-optimization.rb
+++ deps/media/image-optimization.rb
@@ -1,12 +1,12 @@
 dep 'Image optimization' do
-  requires 'imageoptim.cask',
-           'imageoptim-cli.managed'
+  requires 'imageoptim.cask'
 end
 
 # A GUI tool for optimizing images
 dep 'imageoptim.cask'
 
 # A CLI for optimizaing images, using several other tools if available
+# TODO disabled because it has no builds for Apple silicon
 dep 'imageoptim-cli.managed' do
   provides 'imageoptim'
 end
