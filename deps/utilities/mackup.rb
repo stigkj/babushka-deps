da2a6b2f35e4276c394118f4b8e5077e67495814
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2022-03-30 09:29:34 +0200

    Switch from manual to Homebrew install of mackup

--- deps/utilities/mackup.rb
+++ deps/utilities/mackup.rb
@@ -1,30 +1,11 @@
 dep 'mackup' do
   requires 'Telia Sky',
-           'Install mackup with easy_install',
-           'Create the XDG .config directory',
+           'homebrew',
+           'mackup.managed',
            'Mackup restore must run before continuing'
 end
 
-dep 'Install mackup with easy_install' do
-  met? { in_path? 'mackup' }
-  meet {
-    log_shell 'Installing mackup', 'easy_install mackup', :spinner => true, :sudo => true
-  }
-end
-
-# v0.9.2 of mackup needs the XDG config directory to be present before restore
-dep 'Create the XDG .config directory' do
-  met? { xdg_config_directory.exists? }
-  meet {
-    log_block "Creating #{xdg_config_directory}" do
-      xdg_config_directory.mkdir
-    end
-  }
-
-  def xdg_config_directory
-    '~/.config'.p
-  end
-end
+dep 'mackup.managed'
 
 dep 'Mackup restore must run before continuing' do
   met? { '~/.git_global'.p.symlink? }
