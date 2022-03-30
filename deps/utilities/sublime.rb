ed59d3c1cc885d04dd84ee1fb8ed7bf6ed41282b
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2022-03-30 23:05:12 +0200

    Sublime has no dev version in homebrew anymore
    Just use the regular version, which seems to include dev builds.

--- deps/utilities/sublime.rb
+++ deps/utilities/sublime.rb
@@ -1,10 +1,10 @@
 dep 'Sublime Text' do
   requires {
-    on :osx, 'sublime-text-dev.cask'
+    on :osx, 'sublime-text.cask'
     # TODO setup for Linux too
     otherwise { unsupported_platform! }
   }
 end
 
-dep 'sublime-text-dev.cask', :for => :osx
+dep 'sublime-text.cask', :for => :osx
 
