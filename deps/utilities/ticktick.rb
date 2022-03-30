de392d8c59b1a7183c3c97c472ff21ce3fa95d0c
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2022-03-30 09:42:55 +0200

    Add TickTick

---
+++ deps/utilities/ticktick.rb
@@ -0,0 +1,9 @@
+dep 'TickTick', :for => :osx do
+  requires 'ticktick.cask'
+end
+
+dep 'ticktick.cask'
+
+# TODO start the application and list what must be done in the settings
+# * start at login
+# * shortcut
