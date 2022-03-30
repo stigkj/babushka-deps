6268aac6e155d7029378bf4284833035ab1724fe
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2022-03-30 10:00:10 +0200

    Fix paths to installation of Google Cloud SDK
    Also stop Babushka run if the SDK is not in path.

--- deps/network/google-cloud-sdk.rb
+++ deps/network/google-cloud-sdk.rb
@@ -1,14 +1,20 @@
 # Installs the Google cloud SDK, including the gcloud tool
 dep 'Google Cloud SDK' do
   requires 'google-cloud-sdk.cask',
-           'google-cloud-sdk.shell_config_ext'
+           'google-cloud-sdk.shell_config_ext',
+           'google-cloud-sdk-path-working'
 end
 
 dep 'google-cloud-sdk.cask'
 
 dep 'google-cloud-sdk.shell_config_ext' do
   content <<-EOF.unindent
-      source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc'
-      source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc'
+      source '/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc'
+      source '/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc'
     EOF
+end
+
+dep 'google-cloud-sdk-path-working' do
+  met? { ENV['PATH'].include? 'google-cloud-sdk' }
+  meet { unmeetable! 'Start a new shell to get Google Cloud SDK in path and run the babushka command again' }
 end
