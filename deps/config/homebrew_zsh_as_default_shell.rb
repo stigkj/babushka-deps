ef44c4e08ad8a2b4ee2f1b5aaf6c47ec38d461c3
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2022-03-30 09:45:27 +0200

    Improve setup of Homebrew zsh as default shell
    Also fix location of Homebrew stuff.

--- deps/config/homebrew_zsh_as_default_shell.rb
+++ deps/config/homebrew_zsh_as_default_shell.rb
@@ -1,18 +1,12 @@
-homebrew_zsh_location = '/usr/local/bin/zsh'
+homebrew_zsh_location = '/opt/homebrew/bin/zsh'
 
 dep 'Homebrew zsh as default shell' do
-  requires 'zsh',
-           'Add Homebrew zsh to legal shells'
+  requires 'zsh'
 
   met? { shell("dscl . -read /Users/#{ENV['USER']} UserShell").include? homebrew_zsh_location }
-  meet { log_shell 'Setting Homebrew zsh as default shell', "chsh -s #{homebrew_zsh_location}" }
-end
-
-dep 'Add Homebrew zsh to legal shells' do
-  met? { '/etc/shells'.p.read.include? homebrew_zsh_location }
-  meet {
-    log_shell 'Adding Homebrew zsh to legal shells',
-              "echo #{homebrew_zsh_location} >> /etc/shells",
+  meet { 
+    log_shell 'Setting Homebrew zsh as default shell', 
+              "dscl . -create /Users/#{ENV['USER']} UserShell #{homebrew_zsh_location}",
               :sudo => true
   }
 end
