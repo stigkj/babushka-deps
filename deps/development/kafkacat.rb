788037280784f4a84b3ac4274eaa0635c088d8c3
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2022-03-30 09:57:29 +0200

    Change to new name (kafkacat --> kcat)

--- deps/development/kafkacat.rb
+++
@@ -1,6 +0,0 @@
-# Non-JVM Kafka producer and consumer
-dep 'kafkacat' do
-  requires 'kafkacat.managed'
-end
-
-dep 'kafkacat.managed'
---
+++ deps/development/kcat.rb
@@ -0,0 +1,6 @@
+# Non-JVM Kafka producer and consumer
+dep 'kcat' do
+  requires 'kcat.managed'
+end
+
+dep 'kcat.managed'
