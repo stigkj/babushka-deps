66b71379794b4cea41bbf8f27b14aa6f3d3e1ad5
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2022-03-30 09:55:39 +0200

    Fix installation of default java
    Cannot use a cask or this anymore.
    
    Also change versions of the other java installations through `asdf`.

--- deps/development/java.rb
+++ deps/development/java.rb
@@ -1,24 +1,32 @@
 dep 'Java' do
-  requires 'java.cask',
+  requires 'java.managed',
            'java8.asdf',
            'java11.asdf',
            'java17.asdf'
 end
 
 # OS X needs a regularly installed jdk also
-dep 'java.cask'
+dep 'java.managed' do
+  # Cannot use the regular brew info check
+  met? { '/Library/Java/JavaVirtualMachines/openjdk.jdk'.p.exists? }
+
+  after {
+    log_shell 'Initiating system Java wrappers',
+              'ln -sfn /opt/homebrew/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk', :sudo => true
+  }
+end
 
 dep 'java8.asdf' do
   installs 'java'
-  version '8'
+  version 'liberica-8u322+6'
 end
 
 dep 'java11.asdf' do
   installs 'java'
-  version '11'
+  version 'liberica-11.0.14.1+1'
 end
 
 dep 'java17.asdf' do
   installs 'java'
-  version '11'
+  version 'liberica-17.0.2+9'
 end
