5eca68c3d6b794a0ea4a9d822c3d4c6d14cf3647
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2022-03-30 23:04:29 +0200

    Fix missing comma in homebrew dep
    This make the dep not download homebrew.

--- deps/utilities/homebrew.rb
+++ deps/utilities/homebrew.rb
@@ -1,6 +1,7 @@
 dep 'homebrew' do
-  requires 'build tools'
+  requires 'build tools',
            'homebrew downloaded'
+
   met? { in_path? 'brew' }
   meet { 
     log_block 'Setup brew environment variables' do
