4b1a7093261f4ed650f6182b3bf427d86fef60bb
Stig Kleppe-Jørgensen <from.github@nisgits.net>
2022-03-30 09:16:39 +0200

    Move all for fixing DHCP bug into 1 file

--- deps/config/fix_hostname_bug.rb
+++ deps/config/fix_hostname_bug.rb
@@ -1,4 +1,10 @@
 dep 'Fix hostname-through-DHCP bug', :for => :osx do
+  requires 'Set host name',
+           'Set local host name',
+           'Set computer name'
+end
+
+dep 'Set host name', :for => :osx do
   hostname = "local.finn.no"
 
   met? {
@@ -6,9 +12,33 @@ dep 'Fix hostname-through-DHCP bug', :for => :osx do
   }
 
   meet {
-    log_block 'Fixing hostname-through-DHCP bug' do
+    log_block "Setting hostname to #{hostname}" do
       shell "echo '127.0.0.1 #{hostname}' >> /etc/hosts", :sudo => true
       shell "scutil --set HostName #{hostname}", :sudo => true
     end
+  }
+end
+
+dep 'Set local host name', :for => :osx do
+  hostname = 'local'
+
+  met? {
+    shell('scutil --get LocalHostName') == hostname
+  }
+
+  meet {
+    log_shell "Setting local host name to #{hostname}", "scutil --set LocalHostName #{hostname}", :sudo => true
+  }
+end
+
+dep 'Set computer name', :for => :osx do
+  computername = ENV['USER']
+
+  met? {
+    shell('scutil --get ComputerName') == computername
+  }
+
+  meet {
+    log_shell "Setting computer name to #{computername}", "scutil --set ComputerName #{computername}", :sudo => true
   }
 end
--- roles/config/osx_config.laptop.rb
+++ roles/config/osx_config.laptop.rb
@@ -1,6 +1,4 @@
 dep 'OS X config for laptop', :for => :osx do
   requires 'OS X config',
-           'Fix hostname-through-DHCP bug',
-           'Set computer name',
-           'Set local host name'
+           'Fix hostname-through-DHCP bug'
 end
